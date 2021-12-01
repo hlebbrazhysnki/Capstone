8a9a2e3c278292927e7fc508369608795e2b3331
JonathanHill513 <91477154+JonathanHill513@users.noreply.github.com>
2021-12-01 15:48:09 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import React from "react";
-import "./style.css";
-import placeholder from './imgs/placeholder.png'
-
-/*HOME PAGE PLACEHOLDER*/
-
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-<h2 className="center title">Attractions</h2>
 
-        <div className="categories center">
-              <div className="container col-lg-4 col-md-3 col-sm-6 col-xs-4">
-                <h2 className="category-title">Food</h2>
-                <a href="#"><img className="category-img" src={placeholder}/></a>
-              </div>
-              <div className="container col-lg-4 col-md-3 col-sm-6 col-xs-4">
-                <h2 className="category-title">Parks</h2>
-                <a href="#"><img className="category-img" src={placeholder}/></a>
-              </div>
-              <div className="container col-lg-4 col-md-3 col-sm-6 col-xs-4">
-                <h2 className="category-title">Nightlife</h2>
-                <a href="#"><img className="category-img" src={placeholder}/></a>
-              </div>
-              <div className="container col-lg-4 col-md-3 col-sm-6 col-xs-4">
-                <h2 className="category-title">Shopping</h2>
-                <a href="#"><a href="#"><img className="category-img" src={placeholder}/></a></a>
-              </div>
-              <div className="container col-lg-4 col-md-3 col-sm-6 col-xs-4">
-                <h2 className="category-title">Entertainment</h2>
-                <a href="#"><img className="category-img" src={placeholder}/></a>
-              </div>
-              <div className="container col-lg-4 col-md-3 col-sm-6 col-xs-4">
-                <h2 className="category-title">Sights</h2>
-                <a href="#"><img className="category-img" src={placeholder}/></a>
-              </div>
-        </div>
-</div>    
+
+    </div>
   );
 }
 
