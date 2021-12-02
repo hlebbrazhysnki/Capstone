c3a3580ee8a3a78ae253a9b33b505e8c1ecc3e94
Hleb Brazhynski <hleb200217@gmail.com>
2021-12-02 11:34:14 -0500

    Pushing NavBar and a Footer.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
 import './App.css';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      
-       
-       
-      
+    <div className="App"> 
+    <Navbar />
+    <br />
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, et. Sapiente, delectus quasi necessitatibus blanditiis at provident, recusandae, velit laudantium assumenda odio fuga error ex! Fuga veniam itaque rem deserunt provident ducimus dicta cupiditate quod cumque? Pariatur ea maxime odit temporibus, cupiditate quia soluta rem.
+    <br />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import "../App.css";
+
+function Navbar() {
+   
+    return (
+
+        
+        <div className="Navbar">
+            <div className="leftSide">
+                <div className="rightSide">
+
+                    <div className="Links">
+                        <ul className=''>
+                            <a href="/home" className='Text-white'>Home</a>
+                            <a href="/attractions" className='Text-white'>Attractions</a>
+                            <a href="/contact" className='Text-white'>Contact</a>
+                            <a href="/about" className='Text-white'>About Us</a>
+
+                        </ul>
+
+                        
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
