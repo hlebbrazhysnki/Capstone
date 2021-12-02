d99711ea032f15405b2b736b165bf97231196587
Hleb Brazhynski <hleb200217@gmail.com>
2021-12-02 11:39:29 -0500

    Pushing NavBar and a Footer.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="App"> 
     <Navbar />
     <br />
     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, et. Sapiente, delectus quasi necessitatibus blanditiis at provident, recusandae, velit laudantium assumenda odio fuga error ex! Fuga veniam itaque rem deserunt provident ducimus dicta cupiditate quod cumque? Pariatur ea maxime odit temporibus, cupiditate quia soluta rem.
     <br />
     <Footer />
-=======
-    <div className="App">
-
-
->>>>>>> 8a9a2e3c278292927e7fc508369608795e2b3331
     </div>
   );
 }
