35247ea5d64809a4311fd5b9e60c69995af3b49c
JonathanHill513 <91477154+JonathanHill513@users.noreply.github.com>
2021-12-10 15:09:14 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import './App.css';
-import React, { Component } from 'react'
-import Navbar from "./components/Navbar"
-import Footer from './components/Footer';
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import About from './components/About'
-// import Contact from './components/Contact'
-import Home from './components/Home'
+import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom"
+import "./style.css";
+import placeholder from './imgs/placeholder.png'
+import Attractions from "./components/Attractions";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Padding from "./components/Padding";
+
+// import Footer from './components/Footer'
+// import FooterStyles from './components/FooterStyles'
 
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-        <Route path='/' component={ Home } />
-        <Route path='/about' component={About} />
-        {/* <Route path='/contact' component={ Contact } /> */}
-       </Switch>
-        <Footer/>
-        
-     </BrowserRouter>
-    
-     
-    )
-  }
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+    <Navbar />
+    <Padding />
+      <Switch>
+      <Route exact path = "/" component ={Home } />
+      <Route path = "/about" component ={About} />
+      <Route path = "/contact" component ={Contact} />
+      <Route path = "/attractions" component ={Attractions} />
+
+    </Switch>
+<Footer/>
+    </div>
+              
+    </BrowserRouter>
+  );
 }
-export default App
 
+export default App;
