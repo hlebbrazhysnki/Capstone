8c5cc5157bdf00511c37d5321b7c4f260fbfb245
Mike <mikedon.wil@gmail.com>
2021-12-14 13:30:44 -0500

    push

--- routes/entertainment.js
+++ routes/entertainment.js
@@ -1,20 +1,46 @@
 const router = require( 'express' ).Router();
-let entertainment = require( '../models/entertainment.model' );
+let Entertainment = require('../models/entertainment.model')
 
 router.route( '/' ).get( ( req, res ) => {
-    entertainment.find()
-        .then( entertainment => res.json( users ) )
+    Entertainment.find()
+        .then(entertainment => res.json( entertainment ) )
         .catch( err => res.status( 400 ).json( 'Error: ' + err ) );
 } );
 
-router.route( '/add' ).post( ( req, res ) => {
-    const username = req.body.entertainment;
-    
-    const newEntertainment = new Entertainment( { entertainment } );
-    
+router.route('/:id').get((req,res) => {
+    Entertainment.findById( req.params.id )
+        .then((entertainment) => {
+            res.json( entertainment )
+        } ).catch( ( err ) => {
+            res.status( 400 ).json( 'Error ' + err )
+        } );
+}).put(( req, res) => {
+    Entertainment.findById(req.params.id).then((entertainment) => {
+        entertainment.name = req.body.name
+        entertainment.description = req.body.description
+        entertainment.website = req.body.website
+        entertainment.imageURL = req.body.imageURL
+    }).catch((err) => {
+        res.status(400).json('Error ' + err)
+    })
+})
+
+
+router.route('/add').post( ( req, res ) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    const imageURL = req.body.imageURL;
+    const website = req.body.website;
+
+    const newEntertainment = new Entertainment({
+        name,
+        description,
+        imageURL,
+        website,
+    } )
+
     newEntertainment.save()
-        .then( () => res.json( 'Entertainment added!' ) )
-        .catch( err => res.status( 400 ).json( 'Error: ' + err ) );
-} );
-
-module.exports = router;
+        .then( () => {res.json( 'Entertainment added' ) })
+        .catch( (err )=> {res.status( 400 ).json( 'Error: ' + err )} );
+})
+    module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;      // server port#
 
 app.use( cors() );                          // middleware allows parsing of json - server will transmit and receive 
 app.use( express.json() );
-app.use( express.static( 'client/build' ) )
+app.use( express.static( 'client/build' ) );
 
 const uri = process.env.ATLAS_URI;          // where database is stored
 
@@ -21,20 +21,22 @@ connection.once( 'open', () => {
     console.log( "MongoDB database connection established successfully" );          // logged once connection established succesfully
 } );
 
-const sportRouter = require( './routes/sports' )
-const nightlifeRouter = require( './routes/nightlife' )
-const shoppingRouter = require( './routes/shopping' )
+const sportRouter = require( './routes/sports' );
+const nightlifeRouter = require( './routes/nightlife' );
+const shoppingRouter = require( './routes/shopping' );
 const entertainmentRouter = require( './routes/entertainment' )
 const artRouter = require( './routes/art'); 
+const diningRouter = require( './routes/dining' );
 
 
 // const contactRouter = require( './routes/contact' )
 
-app.use( '/sports', sportRouter )
-app.use( '/nightlife', nightlifeRouter )
-app.use( '/shopping', shoppingRouter )
-app.use( 'entertainment', entertainmentRouter )
-app.use( 'art', artRouter );
+app.use( '/sports', sportRouter );
+app.use( '/nightlife', nightlifeRouter );
+app.use( '/shopping', shoppingRouter );
+app.use( '/entertainment', entertainmentRouter );
+app.use( '/art', artRouter );
+app.use( '/dining', diningRouter );
 // app.use( '/contact', contactRouter )
 
 /* Production */
@@ -51,6 +53,7 @@ if ( process.env.NODE_ENV === 'production' ) {
 // const museumsRouter = require( './routes/museums' );
 // const shoppingRouter = require( './routes/shopping,js' );
 // const sportsRouter = require( './routes/sports' );
+// const diningRouter = require( './routes/dining' );
 
 // app.use( 'all', allRouter );
 // app.use( 'entertainment', entertainmentRouter );
@@ -59,7 +62,7 @@ if ( process.env.NODE_ENV === 'production' ) {
 // app.use( 'sports', sportsRouter );
 // app.use( 'shopping', shoppingRouter );
 // app.use( 'art', artRouter );
-
+// app.use( 'dining', diningRouter );
 
 
 app.listen( port, () => {                   // starts the server | begins listening to the aforementioned port 5001
