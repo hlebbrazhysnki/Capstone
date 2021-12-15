969f6bea22498f77cdccc341895bbb38392143bd
Mike <mikedon.wil@gmail.com>
2021-12-15 14:26:57 -0500

    push to heroku

--- client/src/config.js
+++ client/src/config.js
@@ -2,4 +2,4 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const BACKEND_URL=process.env.NODE_ENV === 'development' ? "http://localhost:5000/" : "https://mike-jon-gleb-cap.herokuapp.com/"
+export const BACKEND_URL = process.env.NODE_ENV === 'development' ? "http://localhost:5000/" : "https://kapstoneproject.herokuapp.com/"
