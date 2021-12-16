007bf7c25b2eca4d43df692f442aa917c0009abc
JonathanHill513 <91477154+JonathanHill513@users.noreply.github.com>
2021-12-16 15:57:44 -0500

    Add files via upload

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,17 +1,20 @@
-                import React from 'react'
+import React from 'react'
 import { Placeholder } from 'react-bootstrap'
 import "../style.css";
 import placeholder from '../imgs/placeholder.png'
+import cincy2 from '../imgs/cincy2.png'
+
 
 const About = () => {
     return (
         <div>
-        <div>
+        <div className="body">
 <div className="text-area">
         <h2 className="title center">About Us</h2>
-        <h3 className="text-box">Welcome to Queen City. The best Cincinnati guide</h3>
-        <img src={placeholder} alt="" className="about-right"/>
+        <h3 className="text-box t-box">Welcome to Queen City. The best Cincinnati guide</h3>
 
+        <img src={cincy2} alt="" className="about-right"/>
+        
         <div className="big-text-box t-box">
         <h3>Cincinnati's Finest Guide:</h3>
         <hr />
@@ -28,15 +31,15 @@ const About = () => {
         {/* Youtube video left */}
         <div id="video1">
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/99k-EAMBuM8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/FfrurRPhJ54" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <p id='video-txt'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, aspernatur et? Quidem corrupti voluptas non quasi perferendis vero laudantium facere harum accusantium veritatis. Expedita ullam aspernatur magnam odit iusto consequuntur animi, ad, eum dolor repellendus facilis doloremque delectus id ut!</p></div>
+        <p id='video-txt' className='t-box text-box'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, aspernatur et? Quidem corrupti voluptas non quasi perferendis vero laudantium facere harum accusantium veritatis. Expedita ullam aspernatur magnam odit iusto consequuntur animi, ad, eum dolor repellendus facilis doloremque delectus id ut!</p></div>
         {/* Youtube video right */}
         <div id="video2">
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/99k-EAMBuM8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/eg19iXclGXM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <p id='video-txt'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, aspernatur et? Quidem corrupti voluptas non quasi perferendis vero laudantium facere harum accusantium veritatis. Expedita ullam aspernatur magnam odit iusto consequuntur animi, ad, eum dolor repellendus facilis doloremque delectus id ut!</p>
+        <p id='video-txt' className='t-box text-box'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, aspernatur et? Quidem corrupti voluptas non quasi perferendis vero laudantium facere harum accusantium veritatis. Expedita ullam aspernatur magnam odit iusto consequuntur animi, ad, eum dolor repellendus facilis doloremque delectus id ut!</p>
         
         </div>
 </div>
