ed0f90c8e674a908de8a6636d42d260ea51327ab
Hleb Brazhynski <hleb200217@gmail.com>
2021-12-16 10:51:10 -0500

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,50 +7,39 @@ import {
   FooterLink,
   Heading,
 } from "./FooterStyles";
-import '../style.css'
-  
+import '../style.css';
 const Footer = () => {
   return (
-    
     <div className='footer'>
     <Box>
-    
-      <h1 style={{ color: "white", 
-                   textAlign: "center", 
+      <h1 style={{ color: "white",
+                   textAlign: "center",
                    marginTop: "-50px" }}>
-                 
-        
       </h1>
       <Container>
         <Row>
           <Column>
             <Heading>Capstone Project</Heading>
-            <a className="text-white">This project was created by a team of students from Kable Academy's Web Development Bootcamp.</a>
+            <a class="text-white">This project was created by a team of students from Kable Academy's Web Development Bootcamp.</a>
           </Column>
           <Column>
             <Heading>Michael Wilson II</Heading>
             <FooterLink href="https://www.linkedin.com/in/michael-w-05299340/">LinkedIn</FooterLink>
             <FooterLink href="https://github.com/MikeD-Wil">GitHub</FooterLink>
-            
           </Column>
           <Column>
             <Heading>Gleb Brazhynski</Heading>
             <FooterLink href="https://www.linkedin.com/in/hleb-brazhynski-406a49219/">LinkedIn</FooterLink>
             <FooterLink href="https://github.com/hlebbrazhysnki">GitHub</FooterLink>
-            
           </Column>
           <Column>
-
             <Heading id='Jonny'>Jonathan Hill</Heading>
             <FooterLink href="https://www.linkedin.com/in/jonathan-hill-202997221/">LinkedIn</FooterLink>
             <FooterLink href="https://github.com/JonathanHill513">GitHub</FooterLink>
-            
           </Column>
-          
         </Row>
-
-        
       </Container>
+      ©2021 Capstone. All Rights Reserved.
     </Box>
     </div>
   );
