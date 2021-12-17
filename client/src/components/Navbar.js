be3c741a7811dae40376790baec3304da907af50
JonathanHill513 <91477154+JonathanHill513@users.noreply.github.com>
2021-12-17 09:47:08 -0500

    Add files via upload

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
                     <div className="Links">
                         <ul className=''>
                             <Link to="/" className='Text-white'>Home</Link>
-                            <Link to="/" className='Text-white'>Attractions</Link>
+                            {/* <Link to="/" className='Text-white'>Attractions</Link> */}
                             <Link to="/contact" className='Text-white'>Contact</Link>
                             <Link to="/about" className='Text-white'>About Us</Link>
                             <Link to="/sport" className='Text-white'>Sports</Link>
